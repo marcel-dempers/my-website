ef9eb9cc69415fda9164a93bcf9d8d109f705f2d
Marcel Dempers <34320559+marcel-dempers@users.noreply.github.com>
2025-09-03 13:25:19 +1000

    cd test

---
+++ infra/scripts/deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# settings
+DEPLOYMENT_SOURCE_DIR="$PWD/src"
+DEPLOYMENT_DEST_DIR="/websites/my-website"
+
+#inputs
+PACKAGE_NUMBER=$1
+
+echo "starting our deployment script..."
+
+# Validate Inputs
+if [ -z "$DEPLOYMENT_SOURCE_DIR" ]; then
+  echo "ERROR: source directory is not set!"
+  exit 1
+fi
+
+if [ -z "$PACKAGE_NUMBER" ]; then
+  echo "ERROR: PACKAGE_NUMBER is not passed in!"
+  exit 1
+fi
+
+if [ -z "$DEPLOYMENT_DEST_DIR" ]; then
+  echo "ERROR: destination directory is not set!"
+  exit 1
+fi
+
+echo "extracting package /home/github/packages/package-${PACKAGE_NUMBER}.tar.gz"
+
+tar -xzf -C $PWD  /home/github/packages/package-${PACKAGE_NUMBER}.tar.gz
+ls -la $PWD
